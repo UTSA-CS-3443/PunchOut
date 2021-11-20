071f370b1aeb25c0046dd674e2594fd6c8788ba4
sunkilmoons <bcassidy50@gmail.com>
2021-11-20 16:04:34 -0600

    add base game loop, base sprite, base controller

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,5 +1,8 @@
 package application;
 	
+import application.controller.BaseController;
+import application.controller.TitleScreenController;
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
@@ -7,6 +10,9 @@ import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
+	
+	private BaseController activeController = new TitleScreenController();
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
@@ -15,6 +21,26 @@ public class Main extends Application {
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
+			
+			
+			/**
+			 * This is the main game loop.
+			 * The active view controller should be referenced in this main class and that controller's update method
+			 * will be called from here.
+			 */
+			new AnimationTimer() {
+
+				/**
+				 * @param now: time in nanoseconds
+				 */
+				@Override
+				public void handle(long now) {
+					activeController.update(now);
+				}
+				
+			}.start();
+			
+			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
---
+++ src/application/controller/TitleScreenController.java
@@ -0,0 +1,16 @@
+package application.controller;
+
+
+/**
+ * Controller for the title screen
+ */
+public class TitleScreenController extends BaseController {
+	
+	
+
+	@Override
+	public void update(long nanoSeconds) {
+
+	}
+
+}
